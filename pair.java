3d92b9a1480461882964ab84358f060ade38aa8b
FrankZh27 <jzho@umich.edu>
2017-12-09 16:38:59 -0500

    Delete pair.java

--- pair.java
+++
@@ -1,9 +0,0 @@
-
-public class pair {
-	public Vertex endPoint;
-	public int distance;
-	public pair(Vertex endPoint, int distance) {
-		this.endPoint = endPoint;
-		this.distance = distance;
-	}
-}
